0efec9787856ada724299c2e6310ff37b6f7790a
Patricia Defez <ptddds@gmail.com>
2019-02-22 18:59:22 +0100

    error

---
+++ starter-code/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Navbar from './components/Navbar';
+// import './App.css';
+
+// export default class App extends Component {}
+// render(){}
+const App = () => {
+    return (
+        <div className="App"> 
+            <Navbar/>
+        </div>
+    );
+}      
