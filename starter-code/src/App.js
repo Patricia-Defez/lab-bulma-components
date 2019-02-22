9da6b8894fdb595382aae247c103b082ec6d82b9
Patricia Defez <ptddds@gmail.com>
2019-02-22 20:12:50 +0100

    Navbar

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import Navbar from './components/Navbar';
 // import './App.css';
 
-// export default class App extends Component {}
-// render(){}
-const App = () => {
-    return (
-        <div className="App"> 
-            <Navbar/>
-        </div>
-    );
-}      
-export default App;
+export default class App extends Component {
+render(){
+
+        return (
+            
+                <Navbar/>
+        
+        );
+    }      
+}
--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
 import App from './App';
