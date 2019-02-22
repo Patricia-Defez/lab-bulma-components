18e3eace4e73ef42c20c6c53cd3c1b6bb34cb54e
Patricia Defez <ptddds@gmail.com>
2019-02-22 21:41:40 +0100

    feat:Form Field without stiling

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import FormField from './components/FormField';
 // import './App.css';
 
 export default class App extends Component {
-render(){
+    render() {
 
         return (
-            
-                <Navbar/>
-        
+            <div className="conteiner">
+                <Navbar />
+                <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+                <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" /> 
+            </div>
         );
-    }      
+    }
 }
--- starter-code/src/components/Navbar.js
+++ starter-code/src/components/Navbar.js
@@ -27,47 +27,19 @@ export default class Navbar extends Component {
                         <a className="navbar-item" href="https://bulma.io/">
                             Home
                         </a>
-                        <div className="navbar-item has-dropdown is-hoverable">
-                            <a className="navbar-link" href="https://bulma.io/documentation/overview/start/">
-                                Docs
-                            </a>
-                            <div className="navbar-dropdown is-boxed">
-                                <a className="navbar-item" href="https://bulma.io/documentation/overview/start/">
-                                    Overview
-                                </a>
-                                <a className="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
-                                    Modifiers
-                                </a>
-                                <a className="navbar-item" href="https://bulma.io/documentation/columns/basics/">
-                                    Columns
-                                </a>
-                                <a className="navbar-item" href="https://bulma.io/documentation/layout/container/">
-                                    Layout
-                                </a>
-                                <a className="navbar-item" href="https://bulma.io/documentation/form/general/">
-                                    Form
-                                </a>
-                                <hr className="navbar-divider" />
-                                <a className="navbar-item" href="https://bulma.io/documentation/elements/box/">
-                                    Elements
-                                </a>
-                                <a className="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
-                                    Components
-                                </a>
-                            </div>
-                        </div>
+                        
                     </div>
 
                     <div className="navbar-end">
                         <div className="navbar-item">
                             <div className="field is-grouped">
                                 <p className="control">
-                                    <a className="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
+                                    <a className="button is-info" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
                                         <span className="icon">
                                             <i className="fab fa-twitter"></i>
                                         </span>
                                         <span>
-                                            Tweet
+                                            Login
                                         </span>
                                     </a>
                                 </p>
@@ -76,7 +48,7 @@ export default class Navbar extends Component {
                                         <span className="icon">
                                             <i className="fas fa-download"></i>
                                         </span>
-                                        <span>Download</span>
+                                        <span>Signup</span>
                                     </a>
                                 </p>
                             </div>
@@ -87,7 +59,7 @@ export default class Navbar extends Component {
 
 
 
-        )
+        );
     
     }
 
